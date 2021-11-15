1fa87122bdc1f353e2b677fb75ddbe12eccf0def
ProMahabub21 <imahbub.1999@gmail.com>
2021-11-15 20:38:02 +0600

    footer design implemented

--- View/footer.php
+++ View/footer.php
@@ -1,12 +1,132 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../View/css/footer.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
     <title></title>
+
+    <style>
+        /* table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th,
+        td {
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: 
+        }
+
+        th {
+            background-color: ;
+            color: white;
+        } */
+
+        /* .block {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            text-align: center;
+            background-color: ;
+            color: white;
+
+        }
+        .one-top{
+            
+            margin: 0px;
+            padding-bottom: 20px;
+        }  */
+    </style>
 </head>
-<body style=" text-align:center;">
-    <p style="bottom:0;left:0; position:fixed; display:inline-block;">Copyright©<?php echo date('Y'); ?> Designed By ABC IT Solution</p>
+
+<body>
+    <!--
+            // //display dark block layout with css
+            // echo '<div class="dark-block">';
+            // echo '<div class="dark-block-content">';
+            // echo '<div class="dark-block-content-header">';
+            // echo '<h1>Order List</h1>';
+            // echo '</div>';
+
+            ?> -->
+    <!-- <footer>
+    <div class="block">
+    <div class="one-top" style="background:">
+            <p><h1>About Us</h1></p>    
+    </div>
+        <p>Copyright©<?php //echo date('Y'); ?> Designed By ABC IT Solution</p>
+        
+    </div>
+    </footer>  -->
+    
+    <div class="container"></div>
+    <footer>
+        <section class="ft-main">
+            <div class="ft-main-item">
+                <h2 class="ft-title">About</h2>
+                <ul>
+                   <li> Doctor’s patient is always dedicated</li>
+                   <li> and determined towards providing the </li>
+                   <li> best experience for it’s users. We provide </li>   
+                   <li> a one-stop solution for all your needs  </li>
+                   <li> starting from online consultancy and </li> 
+                   <li> prescription to ordering medicines online.</li>
+                </ul>
+            </div>
+            <div class="ft-main-item">
+                <h2 class="ft-title">Our Services</h2>
+                <ul>
+                    <li>Online Consultation</li>
+                    <li>Expert Doctors</li>
+                    <li>Online Prescription</li>
+                    <li>Online Payment</li>
+                    <li>Online Appointment</li>
+                    <li>Online Medicine Order</li>
+                </ul>
+            </div>
+            <div class="ft-main-item">
+                <h2 class="ft-title">Contact Us</h2>
+                <ul>
+                    <li>Admin Department</li>
+                    <li>+8801750-729276</li>
+                    <li>Email</li>
+                    <li>sasaharukh@gmail.com</li>
+                    <li>HR Department</li>
+                    <li>+8801789-171191</li>
+
+                </ul>
+        </section>
+
+        <section class="ft-social">
+            <ul class="ft-social-list">
+                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-github"></i></a></li>
+                <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+            </ul>
+        </section>
+
+        <section class="ft-legal">
+            <ul class="ft-legal-list">
+                <li><a href="">Privacy Policy</a></li>
+                <li><a href="">Terms of Use</a></li>
+                <li>Copyright ©<?php echo date('Y')?>. All Rights Reserved.Designed By ABC IT Solution</li>
+            </ul>
+        </section>
+
+    </footer>
+
 </body>
+
 </html>
